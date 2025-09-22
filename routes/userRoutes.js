416a0a7343c727be709652b6f59140f68d063c02
sourabh choudhary <113441171+sourabh07032000@users.noreply.github.com>
2025-09-22 09:49:16 +0000

    second

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -15,4 +15,9 @@ router.post("/login-verify", async(req, res)=>{
     res.send(user._id)
 })
 
+router.get("/:id", async(req, res)=>{
+    const user = await User.findById(req.params.id)
+    res.send(user)
+})
+
 module.exports = router;
